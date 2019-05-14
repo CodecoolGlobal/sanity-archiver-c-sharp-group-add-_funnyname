f2755a463708b2e24f21a8185279c3e75b350b3d
mateBalint <matebalint98@gmail.com>
2019-05-14 18:39:30 +0200

    Stylecop settings

---
+++ SanityArchiver/SanityArchiver.Application/Models/Archiver/FileArchiver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanityArchiver.Application.Models.Archiver
+{
+    /// <summary>
+    /// test
+    /// </summary>
+    public class FileArchiver
+    {
+        public void Compress(FileInfo file, string path)
+        {
+        }
+
+        public void Decompress(FileInfo file, string path)
+        {
+        }
+
+        public void ArchiveToIsoStorage(FileInfo file)
+        {
+        }
+    }
+}
