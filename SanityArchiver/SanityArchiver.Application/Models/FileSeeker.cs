06ed2a4c49631c42a6091d98cfc2cc388aa6d096
AlinaGoor <41553929+AlinaGoor@users.noreply.github.com>
2019-05-14 15:35:11 +0200

    FileSeeker and Tests

---
+++ SanityArchiver/SanityArchiver.Application/Models/FileSeeker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanityArchiver.Application.Models
+{
+    /// <summary>
+    /// Search among the files whit a given directory path and pattern.
+    /// The search is recursive in the selected folder.
+    /// </summary>
+    public static class FileSeeker
+    {
+        public static (FileInfo[],int) Search(string root, string pattern)
+        {
+            var directory = new DirectoryInfo(root);
+            FileInfo[] files = null;
+            int numberOfResult = 0;
+            try
+            {
+                files = directory.GetFiles(pattern, SearchOption.AllDirectories);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                numberOfResult = (int)files.Length;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+            }
+            
+
+            return (files,numberOfResult);
+        }
+    }
+}
---
+++ SanityArchiver/SanityArchiver.Tests/FileSeekerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SanityArchiver.Application.Models;
+
+namespace SanityArchiver.Tests
+{
+    [TestClass]
+    public class FileSeekerTest
+    {
+        [TestMethod]
+        public void GetFileForSearch()
+        {
+            string path = @"C:\Users\sfarkas\Desktop\CSharp_Udemy";
+            string fileName = "Loops.cs";
+            FileInfo[] files = new DirectoryInfo(path).GetFiles(fileName);
+            var result = FileSeeker.Search(path, fileName);
+            Assert.IsTrue(files.Length == result.Item1.Length);
+        }
+
+        [TestMethod]
+        public void GetFileForRecursiveSearch()
+        {
+            string path = @"C:\Users\sfarkas\Desktop";
+            string fileName = "Loops.cs";
+            var result = FileSeeker.Search(path, fileName);
+            Assert.IsNotNull(result.Item2);
+        }
+    }
+}
