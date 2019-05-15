bd3a4251d1a7b51dfa3c1f49e7fe57746ecb8fbb
MenesiZoltan <colos.menesi@gmail.com>
2019-05-15 10:09:21 +0200

    Implemented FileHandler Class + added documentation

---
+++ SanityArchiver/SanityArchiver.Application/Models/FileHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace SanityArchiver.Application.Handler
+{
+    public class FileHandler
+    {
+
+        public void Move(FileInfo file, string destination)
+        {
+            file.MoveTo(destination);
+        }
+
+        public void Copy(FileInfo file, string destination)
+        {
+            ///this will create a copy 
+        }
+
+        public void ChangeName(FileInfo file, string newName)
+        {
+            ///this will update the file's name
+        }
+
+        public void ChangeExtension(FileInfo file, string extension)
+        {
+            ///this will update the extension of the file
+        }
+
+        public void ModifyVisibility(FileInfo file, bool visible)
+        {
+            ///this will update the visibility of the file
+        }
+
+    }
+}
